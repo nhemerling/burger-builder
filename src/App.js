dcabb0c50222dbda45afb52a58141174267394a7
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-03-22 16:12:35 -0700

    install react-router-dom as dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,12 +8,14 @@ import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-          <Checkout />
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Layout>
+            <BurgerBuilder />
+            <Checkout />
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
 }
