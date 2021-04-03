b3f230e83a0ae2a247d26c45063b4ee6138f5c86
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2021-04-03 10:18:53 -0700

    add auth in redux add Auth component to signin and signup users connect auth to firebase to signup users

---
+++ src/store/actions/auth.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD_duFl_rV0eEvJTYSHWr7D3qd67EUrU-U';
+    if (!isSignup) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_duFl_rV0eEvJTYSHWr7D3qd67EUrU-U';
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
+  };
+};
